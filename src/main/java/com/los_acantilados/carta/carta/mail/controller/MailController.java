0a3f386adba38296d98b3cd790d4d036a21f83d3
David L <david.a.lencina@gmail.com>
2022-06-11 18:48:23 -0300

    Cambios en plantilla de email

--- src/main/java/com/los_acantilados/carta/carta/mail/controller/MailController.java
+++ src/main/java/com/los_acantilados/carta/carta/mail/controller/MailController.java
@@ -7,6 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
 
 @Controller
 @RequestMapping("/email")
@@ -18,12 +22,10 @@ public class MailController {
     public MailController(MailService mailService) {
         this.mailService = mailService;
     }
+    @Autowired
+    private TemplateEngine templateEngine;
 
-    /*   @GetMapping("/")
-       public String index(){
-           return "index.html";
-       }
-   */
+/*
     @PostMapping("/sendMail")
     public ResponseEntity<MailDTO> sendMail(@RequestBody MailDTO mailDTO) {
         String subject = "Comentarios desde tu Menú Web";
@@ -33,8 +35,27 @@ public class MailController {
 
         //TODO: poner como TO el correo de Los Acantilados
         String newMessage = "Mensaje: \n   " + message + "\n\nDatos de contacto: " + "\nNombre: " + name + "\nEmail: " + email;
-        mailService.sendMail("senderlosacantilados@hotmail.com", "senderlosacantilados@hotmail.com", subject, newMessage);
+        mailService.sendSimpleMail("senderlosacantilados@hotmail.com", "senderlosacantilados@hotmail.com", subject, newMessage);
         return ResponseEntity.status(HttpStatus.OK).body(mailDTO);
     }
+*/
 
+    @PostMapping("/sendMail")
+    public ResponseEntity<MailDTO> sendMail(@RequestBody MailDTO mailDTO) throws MessagingException {
+        //TODO: convertir los datos del DTO en variables de entorno de thymeleaf
+        String subject = "Comentarios desde tu Menú Web";
+        String message = mailDTO.getMessage();
+        String name = mailDTO.getName();
+        String email = mailDTO.getEmail();
+
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("name", name);
+        context.setVariable("email", email);
+        String htmlContext = templateEngine.process("contact-email-template", context);
+
+        mailService.fileTextMessage(htmlContext);
+        return ResponseEntity.status(HttpStatus.OK).body(mailDTO);
+
+    }
 }
--- src/main/java/com/los_acantilados/carta/carta/mail/service/MailService.java
+++ src/main/java/com/los_acantilados/carta/carta/mail/service/MailService.java
@@ -3,28 +3,43 @@ package com.los_acantilados.carta.carta.mail.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.util.UUID;
+
 
 @Service
 public class MailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+
     public MailService(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
-
-    public void sendMail (String from, String to, String subject, String message){
-
+/*
+    public void sendSimpleMail (String from, String to, String subject, String message){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
-
         mailMessage.setFrom(from);
         mailMessage.setTo(to);
         mailMessage.setSubject(subject);
         mailMessage.setText(message);
-
         javaMailSender.send(mailMessage);
     }
+*/
+    public void fileTextMessage(String htmlContext) throws MessagingException{
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true,"UTF-8");
 
+        messageHelper.setTo("senderlosacantilados@hotmail.com");
+        messageHelper.setFrom("senderlosacantilados@hotmail.com");
+        messageHelper.setSubject("Comentarios desde tu Menú Web");
+        messageHelper.setText(htmlContext, true);
+        this.javaMailSender.send(mimeMessage);
+    }
 
 }
